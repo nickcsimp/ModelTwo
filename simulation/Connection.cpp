4fe5cb2895ec79781092ef55d9a83137d964e788
Nicholas Simpson <nicksimpson@Nicholass-MBP.home>
2022-03-31 18:41:24 +0100

    Connection correction

--- simulation/Connection.cpp
+++ simulation/Connection.cpp
@@ -7,13 +7,13 @@
 bool Connection::operator==(Connection c){
 
     if(*polymers_in_connection[0] == *c.polymers_in_connection[0] && *polymers_in_connection[1] == *c.polymers_in_connection[1]){
-        if(indexes[0] == c.indexes[0] && indexes[1] == indexes[1]){
+        if(indexes[0] == c.indexes[0] && indexes[1] == c.indexes[1]){
             return true;
         }
     }
 
     if(*polymers_in_connection[1] == *c.polymers_in_connection[0] && *polymers_in_connection[0] == *c.polymers_in_connection[1]){
-        if(indexes[1] == c.indexes[0] && indexes[0] == indexes[1]){
+        if(indexes[1] == c.indexes[0] && indexes[0] == c.indexes[1]){
             return true;
         }
     }
